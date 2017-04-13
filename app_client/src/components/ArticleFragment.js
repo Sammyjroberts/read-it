557279a0aca996d651c336c34fb7f902bf05f1c1
sammy roberts <sammy.roberts@outlook.com>
2017-04-13 16:54:31 -0700

    article fragment

---
+++ app_client/src/components/ArticleFragment.js
@@ -0,0 +1,15 @@
+/**
+ * Created by sammy on 4/13/17.
+ */
+import React from 'react';
+
+
+class ArticleFragment extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
